5e4e1f3e1cef7ef8c3772acff300489da28da967
woodske <woodske@oregonstate.edu>
2017-07-12 01:37:25 +0000

    Updated homepage route to show best bot rankings. Added two extra routes

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ const mysql = require('mysql');
 const express = require('express');
 const app = express();
 const script = require('./public/scripts/script.js');
+const bodyParser = require('body-parser');
 
 /**
  * Declare a static directory to access stylesheets
  * */
 app.use(express.static(__dirname + '/public'));
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 /**
  * Connect to Database. Hosted at AWS
@@ -21,16 +26,59 @@ const con = mysql.createConnection({
 /**
  * Every 2.15 seconds, call scrape() to search Reddit for new comments
  * */
-// setInterval(function() {
-//   script.scrape();
-// }, 2150);
+setInterval(function() {
+  script.scrape();
+}, 2150);
 
 /**
  * Homepage. Display the top 6 in most good bot votes and bad bot votes
  * */
 app.get('/', function (req, res) {
-    res.render('home.ejs');
+    
+    var total;              //Total number of bot votes
+    var goodCount;          //Total number of good bot votes
+    var badCount;           //Total number of bad bot votes
+    var botNameArr = [];    //Array of bot names
+    var botScoreArr = [];   //Array of confidence interval scores
+    
+    var sql = "SELECT SUM(goodCount), SUM(badCount), SUM(goodCount) + SUM(badCount) FROM bot;";
+    con.query(sql, function(err, result) {
+        if (err) {
+            throw (err);
+        }
+        total = result[0]['SUM(goodCount) + SUM(badCount)'];
+        goodCount = result[0]['SUM(goodCount)'];
+        badCount = result[0]['SUM(badCount)'];
+    });
+    
+    var sql = "SELECT botName, ((goodCount + 1.9208) / (goodCount + badCount) - " +
+                "1.96 * SQRT((goodCount * badCount) / (goodCount + badCount) + 0.9604) / " +
+                "(goodCount + badCount)) / (1 + 3.8416 / (goodCount + badCount)) " +
+                "AS ci_lower_bound FROM bot WHERE goodCount + badCount > 0 " +
+                "ORDER BY ci_lower_bound DESC limit 6;";
+
+    con.query(sql, function(err, result) {
+        if (err) 
+            throw (err);
+            
+        result.forEach(function(key) {
+            botNameArr.push(key.botName);
+            botScoreArr.push(key.ci_lower_bound);
+        });
+        
+        res.render('home.ejs', 
+            {
+                total: total,
+                goodCount: goodCount,
+                badCount: badCount,
+                botName: botNameArr,
+                botScore: botScoreArr
+            }
+        );
+    });
 });
+
+
 
 app.get('/good_filter', function (req, res) {
     var botNameArr = [];
@@ -73,6 +121,40 @@ app.get('/bad_filter', function (req, res) {
                 botVote: botVoteArr
             }
         );  
+    });
+});
+
+app.get('/all_filter', function(req, res) {
+    
+    var byGoodArr = [];
+    
+    var sql = "SELECT botName, goodCount, badCount FROM bot WHERE goodCount > 0 ORDER BY goodCount DESC;";
+    con.query(sql, function(err, result) {
+        if (err) {
+            throw (err);
+        }
+        byGoodArr = result;
+    });
+    
+    var sql = "SELECT botName, goodCount, badCount FROM bot WHERE badCount > 0 ORDER BY badCount DESC;";
+    con.query(sql, function(err, result) {
+        if (err) {
+            throw (err);
+        }
+        res.render('all_filter.ejs', {byGoodArr: byGoodArr, byBadArr: result});
+    });
+});
+
+app.post('/voter', function(req, res) {
+    
+    var sql = "SELECT botName FROM bot INNER JOIN bot_voter ON bot.bot_id = bot_voter.bot_id " +
+        "INNER JOIN voter ON bot_voter.voter_id = voter.voter_id " +
+        "WHERE voter.voterName = '" + req.body.voter + "';";
+    con.query(sql, function(err, result) {
+        if (err) {
+            throw (err);
+        }
+        res.render('voter.ejs', {voter: req.body.voter, bot: result});
     });
 });
 
