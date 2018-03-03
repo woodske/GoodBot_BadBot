ec8e1fd3c4bd16905f4436985054b4bde6354654
woodske <woodske@oregonstate.edu>
2018-03-03 00:33:55 +0000

    updated for current schema

--- public/setup.sql
+++ public/setup.sql
@@ -18,10 +18,24 @@ CREATE TABLE bot_voter (
     bot_id int(11) NOT NULL,
     voter_id int(11) NOT NULL,
     PRIMARY KEY (bot_id, voter_id),
+    vote varchar(255),
+    time varchar(255),
+    vote_hour int(11),
+    vote_day int(11),
+    vote_month int(11),
+    vote_year int(11),
     FOREIGN KEY (bot_id) REFERENCES bot (bot_id)
         ON UPDATE CASCADE
         ON DELETE CASCADE,
     FOREIGN KEY (voter_id) REFERENCES voter (voter_id)
         ON UPDATE CASCADE
         ON DELETE CASCADE
+) ENGINE=InnoDB;
+
+CREATE TABLE link (
+    link_id int(11) NOT NULL PRIMARY KEY
+) ENGINE=InnoDB;
+
+CREATE TABLE skipOver (
+    userName varchar(255) NOT NULL PRIMARY KEY
 ) ENGINE=InnoDB;
